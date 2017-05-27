fb1cf5b6cd518eef2389aa7577ab646aaed25e55
Panagiotis Papadimitropoulos <ppapadim12@gmail.com>
2017-05-27 23:13:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array_list)
+  index = 0
+  modified_arr = []
+  while index < array_list.size
+    modified_arr << yield(array_list[index])
+    index += 1
+  end
+  modified_arr
+end
